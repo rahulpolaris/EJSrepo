55ddd19d3f53640ec9efe9e35e785469def8c315
rahulpolaris <iamrahuljin@gmail.com>
2020-12-01 20:25:46 +0530

    just added functionality of title and parameters being linked in address rerereress bar

--- personalBlog/app.js
+++ personalBlog/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const date = require(__dirname + "/date.js");
+const lowerCase = require("lodash/lowerCase");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutStartingContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -29,12 +30,6 @@ res.render(a, {foo: "home", pageContent: homeStartingContent, contentArrays: blo
 
 console.log("this should be path"+req.route.path)
 console.log("this should be id: "+req.query.id)
-
-
-
-
-
-
 
 });
 //rendering about page.................................
@@ -82,13 +77,18 @@ app.get("/post/:topic",(req,res)=>
   console.log("this is the parameter:  >"+pmtr);
   // const result = blogPostArrays.find(({blogTitle})=>{blogTitle==="football";});
   blogPostArrays.forEach((blogPostArray)=>{
-    if(blogPostArray.blogTitle === pmtr)
+    if(lowerCase(blogPostArray.blogTitle) === lowerCase(pmtr))
     {
       compareParam = 1;
+      res.render("post",{postsTitle:blogPostArray.blogTitle, postsMessage:blogPostArray.blogMessage})
     }
 
 });
-if(compareParam){ console.log("parameter found");}
+if(compareParam)
+{
+   console.log("parameter found");
+  //  res.render("posts",{postsTitle:blogPostArray.blogTitle, postsMessage:blogPostArray.blogMessage})
+  }
 else{console.log("parameter not found");}
 
  
